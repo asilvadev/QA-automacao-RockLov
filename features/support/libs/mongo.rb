8c817e6df3eace4112189080b957b32d64f23f14
Alan Silva <alan.silva@imd.ufrn.br>
2021-01-14 11:20:53 -0300

    remove massa de teste do db antes da submissão | index 5-26

--- features/support/libs/mongo.rb
+++ features/support/libs/mongo.rb
@@ -9,6 +9,22 @@ Mongo::Logger.logger = Logger.new("./logs/mongo.log")
             users = client[:users]
             users.delete_many({ email: "alan@temp.vc" })
         end
+
+        def get_user(email)
+            client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
+            users = client[:users]
+            user = users.find({email: email}).first
+            return user[:_id]
+        end
+        
+        def remove_equipos(name, email)
+            user_id = get_user(email)
+            client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
+            equipos = client[:equipos]
+            equipos.delete_many({name: name, user: user_id})
+        end
+
+
         
     end
 
