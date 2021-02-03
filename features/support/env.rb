b464e80d90d72f13c040a3d602f7ad7a776bc5be
Alan Silva <alan.silva@imd.ufrn.br>
2021-02-03 15:57:00 -0300

    [SET] allure config

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,14 @@
 require "capybara"
 require "capybara/cucumber"
+require "allure-cucumber"
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.app_host = "http://127.0.0.1:3000"
   config.default_max_wait_time = 10
+end
+
+AllureCucumber.configure do |config|
+  config.results_directory = "/logs"
+  config.clean_results_directory = true
 end
