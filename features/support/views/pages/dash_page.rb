5042f50a281233b2b0aaa0f90df81a7e6d53f3f8
Alan Silva <alan.silva@imd.ufrn.br>
2021-02-03 16:00:14 -0300

    [Format Doc Ruby] auto save style format

--- features/support/views/pages/dash_page.rb
+++ features/support/views/pages/dash_page.rb
@@ -1,17 +1,15 @@
-
 class DashPage
-    include Capybara::DSL
+  include Capybara::DSL
 
-    def ondash?
-        return page.has_css?(".dashboard")
-    end
+  def ondash?
+    return page.has_css?(".dashboard")
+  end
 
-    def goto_equipo_form
-        click_button "Criar anúncio"
-    end
+  def goto_equipo_form
+    click_button "Criar anúncio"
+  end
 
-    def equipo_list
-        return find(".equipo-list")
-    end
-    
+  def equipo_list
+    return find(".equipo-list")
+  end
 end
