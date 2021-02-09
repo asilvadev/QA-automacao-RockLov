40e514df7195c3085c7a144e1b5596155d820610
Alan Silva <alan@pop-os.localdomain>
2021-02-09 19:35:28 -0300

    [Hooks] Take screenshot to Allure report

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -7,3 +7,13 @@ Before do
 
   page.current_window.resize_to(1440, 900)
 end
+
+After do
+  temp_shot = page.save_screenshot("logs/temp_screenshot.png")
+
+  Allure.add_attachment(
+    name: "screenshot",
+    type: Allure::ContentType::PNG,
+    source: File.open(temp_shot),
+  )
+end
