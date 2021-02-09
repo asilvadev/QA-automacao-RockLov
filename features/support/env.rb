300884761c93daa573ac3207a1214312b6105e62
Alan Silva <alan@pop-os.localdomain>
2021-02-09 19:53:03 -0300

    [CONFIG] nova configuração para suporte multi-ambiente  {gitkraken}

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,9 +2,11 @@ require "capybara"
 require "capybara/cucumber"
 require "allure-cucumber"
 
+CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
+
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
-  config.app_host = "http://127.0.0.1:3000"
+  config.app_host = CONFIG["url"]
   config.default_max_wait_time = 10
 end
 
