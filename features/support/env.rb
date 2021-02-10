8f75ad298bc1213a54709c65b67895711262a661
Alan Silva <alan@pop-os.localdomain>
2021-02-09 21:42:20 -0300

    [ENV] set Support multi browser. Default chrome_headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,8 +4,21 @@ require "allure-cucumber"
 
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
 
+case ENV["BROWSER"]
+when "firefox"
+  @driver = :selenium
+when "firefox_headless"
+  @driver = :selenium_headless
+when "chrome"
+  @driver = :selenium_chrome
+when "chrome_headless"
+  @driver = :selenium_chrome_headless
+else
+  raise "Invalid Browser. Undefined @drive"
+end
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = @driver
   config.app_host = CONFIG["url"]
   config.default_max_wait_time = 10
 end
