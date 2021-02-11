eca7092d8d15712a3c9b854fa8c90d63de08f75d
Alan Silva <alan.silva@imd.ufrn.br>
2021-02-11 13:25:29 -0300

    mod specs

--- api/spec/post_session_spec.rb
+++ api/spec/post_session_spec.rb
@@ -1,6 +1,6 @@
 require_relative "routes/sessions"
 
-describe "POST /sessrion" do
+describe "POST /session" do
   context "login com sucesso" do
     before(:all) do
       payload = {
@@ -18,21 +18,62 @@ describe "POST /sessrion" do
       expect(@result.parsed_response["_id"].length).to eql 24
     end
   end
-  context "senha invalida" do
-    before(:all) do
-      payload = {
-        email: "alan2@temp.vc",
-        password: "1pwd123",
-      }
-      @result = Sessions.new.login(payload)
-    end
 
-    it "validar status code" do
-      expect(@result.code).to eql 401
-    end
+  exemples = [
+    {
+      title: "Wrong Password",
+      payload: { email: "alan2@temp.vc", password: "1pwd123" },
+      code: 401,
+      error: "Unauthorized",
+    },
+    {
+      title: "Wrong Email",
+      payload: { email: "404@temp.vc", password: "1pwd123" },
+      code: 401,
+      error: "Unauthorized",
+    },
+    {
+      title: "Blank Email input",
+      payload: { email: "", password: "1pwd123" },
+      code: 412,
+      error: "required email",
+    },
+    {
+      title: "Undefined Email",
+      payload: { password: "1pwd123" },
+      code: 412,
+      error: "required email",
+    },
+    {
+      title: "Blank Password input",
+      payload: { email: "temp", password: "" },
+      code: 412,
+      error: "required password",
+    },
+    {
+      title: "Undefined Password",
+      payload: { email: "temp" },
+      code: 412,
+      error: "required password",
+    },
+  ]
+  # puts exemples.to_json #gerar estrutura em json para converter em yml
 
-    it "validar error" do
-      expect(@result.parsed_response["error"]).to eql "Unauthorized"
+  # exemples = YAML.load(File.read(Dir.pwd + "/spec/fixtures/login.yml", symbolize_names: true))
+
+  exemples.each do |e|
+    context "#{e[:title]}" do
+      before(:all) do
+        @result = Sessions.new.login(e[:payload])
+      end
+
+      it "validar status code #{e[:code]}" do
+        expect(@result.code).to eql e[:code]
+      end
+
+      it "validar error #{e[:error]}" do
+        expect(@result.parsed_response["error"]).to eql e[:error]
+      end
     end
   end
 end
