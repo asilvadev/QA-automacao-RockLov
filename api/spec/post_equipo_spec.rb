860df6f9269748b0020ef4603f8ce5478d7547fb
Alan Silva <alan.silva@imd.ufrn.br>
2021-02-19 09:23:53 -0300

    cenario de cadastro de anuncio nao autorizado

--- api/spec/post_equipo_spec.rb
+++ api/spec/post_equipo_spec.rb
@@ -25,4 +25,25 @@ describe "POST /equipos" do
       expect(@result.code).to eql 200
     end
   end
+
+  context "nao autorizado" do
+    before(:all) do
+      thumbnail = File.open(File.join(Dir.pwd, "spec/fixtures/images", "baixo.jpg"), "rb")
+
+      payload = {
+        thumbnail: thumbnail,
+        name: "Contra Baixo",
+        category: "Cordas",
+        price: 59,
+      }
+
+      # MongoDB.new.remove_equipo(payload[:name], @user_id)
+
+      @result = Equipos.new.create(payload, nil)
+    end
+
+    it "deve retornar 401" do
+      expect(@result.code).to eql 401
+    end
+  end
 end
