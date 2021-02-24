cec37fa52ecc2de7886aad8e002d378911a04135
Alan Silva <alan.silva@imd.ufrn.br>
2021-02-24 09:07:36 -0300

    test remove equipo com implementação em page_object

--- web/features/step_definitions/anuncio_steps.rb
+++ web/features/step_definitions/anuncio_steps.rb
@@ -35,24 +35,28 @@ Dado("que eu tenho um anuncio indesejado:") do |table|
 
   thumbnail = File.open(File.join(Dir.pwd, "features/support/fixtures/images", table.rows_hash[:thumb]), "rb")
 
-  equipo = {
+  @equipo = {
     thumbnail: thumbnail,
     name: table.rows_hash[:nome],
     category: table.rows_hash[:categoria],
     price: table.rows_hash[:preco],
   }
 
-  EquiposService.new.create(equipo, user_id)
+  EquiposService.new.create(@equipo, user_id)
+
+  visit current_path #reload page after api inject equipo
 end
 
 Quando("eu solicito a exclusão desse item") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @dash_page.request_removal(@equipo[:name])
+
+  sleep 1 #think time (simulação do usuario)
 end
 
 Quando("confirmo a exclusão") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @dash_page.confirm_removal
 end
 
 Então("não devo ver esse item no meu Dashboard") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(@dash_page.has_no_equipo?(@equipo[:name])).to be true
 end
--- web/features/support/views/pages/dash_page.rb
+++ web/features/support/views/pages/dash_page.rb
@@ -12,4 +12,17 @@ class DashPage
   def equipo_list
     return find(".equipo-list")
   end
+
+  def request_removal(name)
+    equipo = find(".equipo-list li", text: name)
+    equipo.find(".delete-icon").click
+  end
+
+  def confirm_removal
+    click_on "Sim"
+  end
+
+  def has_no_equipo?(name)
+    return page.has_no_css?(".equipo-list li", text: name)
+  end
 end
