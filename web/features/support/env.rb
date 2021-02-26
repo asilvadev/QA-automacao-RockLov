c5a5d79532fd89f4cb37132febc374b051446915
Alan Silva <alan.silva@imd.ufrn.br>
2021-02-26 09:57:38 -0300

    Implementação de compatibilidade para o container

--- web/features/support/env.rb
+++ web/features/support/env.rb
@@ -12,6 +12,19 @@ when "firefox_headless"
 when "chrome"
   @driver = :selenium_chrome
 when "chrome_headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    version = Capybara::Selenium::Driver.load_selenium
+    options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
+    browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+      opts.add_argument("--headless")
+      opts.add_argument("--disable-gpu")
+      opts.add_argument("--disable-site-isolation-trials")
+      opts.add_argument("--no-sandbox")
+      opts.add_argument("--disable-dev-shm-usage")
+    end
+
+    Capybara::Selenium::Driver.new(app, **Hash[:browser => :chrome, options_key => browser_options])
+  end
   @driver = :selenium_chrome_headless
 else
   raise "Invalid Browser. Undefined @drive"
